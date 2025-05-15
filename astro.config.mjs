95db91fd6c7bb922575aae2693fda4d985cde56a
Andres carvajal <127360563+AndrewCarvajal97@users.noreply.github.com>
2025-05-15 18:35:14 -0500

    ajuste  ci/cd

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
-  site: 'https://andrescarvajal.dev',
+  base: '/',
+  site: 'https://andrewcarvajal.dev/',
   integrations: [react(), tailwind()],
 });
